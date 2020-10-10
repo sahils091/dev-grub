5aa124aacfa9d8ae7b305ac82f697e342287549a
Sahil Singh <sahils091@gmail.com>
2020-10-09 20:52:49 -0400

    Added a a Bootstrap Navbar/ Input Component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import "./header.css";
+
+
+const Header = () => {
+    return (
+        <header className="head">
+
+<Container className="p-0" fluid={true}>
+        <Navbar className="border-bottom" bg="transparent" expand="lg">
+        <Navbar.Brand>Dev Grub</Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="navbar-toggle" className="border-0" />
+        <Navbar.Collapse id="navbar-toggle">
+          <Nav className="ml-auto">
+            <a className="nav-link" href="#home">
+              {" "}
+              Home{" "}
+            </a>
+            <a className="nav-link" href="#about">
+              About Us
+            </a>
+            <a className="nav-link" href="#projects">
+              Get In Touch
+            </a>
+            <a className="nav-link" href="#contact">
+            Why Dev-Grub
+            </a>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+        </Container>
+     </header>        
+        
+    );
+};
+
+export default Header;
--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import Button from "@material-ui/core/Button";
 import SearchIcon from "@material-ui/icons/Search";
 import TextField from "@material-ui/core/TextField";
+import Header from "../Header/Header";
+import Input from "../Input/Input";
 
 
 
@@ -17,8 +19,11 @@ const Homepage = () => {
     axios
       .get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + key)
       .then((res) => {
-        console.log(res.data.meals);
-        let meal = res.data.meals[0];
+        let meal;
+        if (res.data.meals === null) {
+          return }else {
+         meal = res.data.meals[0];
+        }
         setKeyword(meal);
       });
   };
@@ -36,22 +41,10 @@ const Homepage = () => {
 
   return (
     <div className="home">
+    <Header/>
       <div className="home__input-wrapper">
-        <form className="home__input-form" onSubmit={clickHandler}>
-          <input type="text" name="search" className="home__input" />
-
-          {/* <TextField
-           onSubmit={clickHandler}
-           variant="standard"
-           type="text"
-           label="Search"
-          ></TextField> */}
-     <Button variant="contained" color="primary" size="small" endIcon={<SearchIcon/>} >
-     Search
-     </Button>
-            
+      <Input clickHandler={clickHandler}/>
       
-        </form>
       </div>
 
       <main>
@@ -67,3 +60,21 @@ const Homepage = () => {
 };
 
 export default Homepage;
+
+
+// {/* <form className="home__input-form" onSubmit={clickHandler}>
+// {/* <input type="text" name="search" className="home__input" /> */}
+
+// <TextField
+// //  onSubmit={clickHandler}
+//  variant="standard"
+//  type="text"
+//  label="Search"
+//  name="search"
+// ></TextField>
+// <Button variant="contained" color="primary" size="small" endIcon={<SearchIcon/>}  type="submit">
+// Search
+// </Button>
+  
+
+// </form> */}
