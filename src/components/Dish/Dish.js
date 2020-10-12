8210639c32944de7a8a1c45f8eab409dbbe82017
Sahil Singh <sahils091@gmail.com>
2020-10-11 20:22:17 -0400

    Added Links to recipe and video/ Minor Css changes

--- src/components/Dish/Dish.js
+++ src/components/Dish/Dish.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./dish.css";
 
 const Dish = (props) => {
-  console.log(props);
   return (
     <section className="dish">
       <div className="dish__wrapper">
@@ -17,8 +16,31 @@ const Dish = (props) => {
         />
         <p className="dish__blurb">
           {" "}
-          Direction: {props.keyword.strInstructions}
+          <strong>Direction : </strong> {props.keyword.strInstructions}
         </p>
+
+        <h4 className="dish__link">
+          {props.keyword.strSource ? (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={props.keyword.strSource}
+            >
+              <strong>Full Recipe Here</strong>
+            </a>
+          ) : null}
+        </h4>
+        <h4 className="dish__video">
+          {props.keyword.strYoutube ? (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={props.keyword.strYoutube}
+            >
+              <strong>Watch the Video</strong>
+            </a>
+          ) : null}
+        </h4>
       </div>
     </section>
   );
