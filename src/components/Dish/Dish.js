c38786efeb303af5952178204b4a2dd793daf269
Sahil Singh <sahils091@gmail.com>
2020-10-16 20:53:06 -0400

    Created scroll animations for the dish component

--- src/components/Dish/Dish.js
+++ src/components/Dish/Dish.js
@@ -1,25 +1,28 @@
 import React from "react";
 import "./dish.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Dish = (props) => {
   return (
     <section className="dish">
-      <div className="dish__wrapper">
-        <h1 className="dish__title"> Name: {props.keyword.strMeal}</h1>
-        <h2 className="dish__subtitle">
+      <div className="dish__wrapper" data-aos="fade-up">
+        <h1 className="dish__title"  data-aos="fade-up-right"> Name: {props.keyword.strMeal}</h1>
+        <h2 className="dish__subtitle"  data-aos="fade-up-right">
           Country Of Origin : {props.keyword.strArea}
         </h2>
         <img
           className="dish__img"
           src={props.keyword.strMealThumb}
           alt={props.keyword.idMeal}
+          data-aos="fade-up-right"
         />
         <p className="dish__blurb">
           {" "}
-          <strong>Direction : </strong> {props.keyword.strInstructions}
+          <strong >Direction : </strong> {props.keyword.strInstructions}
         </p>
 
-        <h4 className="dish__link">
+        <h4 className="dish__link"  data-aos="fade-up-right">
           {props.keyword.strSource ? (
             <a
               target="_blank"
@@ -30,7 +33,7 @@ const Dish = (props) => {
             </a>
           ) : null}
         </h4>
-        <h4 className="dish__video">
+        <h4 className="dish__video"  data-aos="fade-up-right">
           {props.keyword.strYoutube ? (
             <a
               target="_blank"
